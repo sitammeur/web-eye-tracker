5d41c9e8be8b704893351ada078ae06cfa50e8f3
Sitam Meur <103279526+sitamgithub-MSIT@users.noreply.github.com>
2024-08-06 21:30:10 +0000

    storage file docs added

--- app/services/storage.py
+++ app/services/storage.py
@@ -1,17 +1,33 @@
+import os
+from pathlib import Path
 from flask import Flask
 from werkzeug.utils import secure_filename
-from pathlib import Path
-import os
 
-UPLOAD_FOLDER = f'{Path().absolute()}/public/videos'
 
+UPLOAD_FOLDER = f"{Path().absolute()}/public/videos"
+
+# Initialize Flask app
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 def save_file_locally(file, folder):
+    """
+    Save a file locally in the specified folder.
+
+    Args:
+        file: The file to be saved.
+        folder: The folder where the file will be saved.
+
+    Returns:
+        The path of the saved file relative to the specified folder.
+    """
     # Create folder if does not exists
-    os.makedirs(UPLOAD_FOLDER+folder, exist_ok=True)
+    os.makedirs(UPLOAD_FOLDER + folder, exist_ok=True)
+
+    # Save file
     filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER']+folder, filename))
-    return f'{folder}/{filename}'
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"] + folder, filename))
+
+    # Return file path
+    return f"{folder}/{filename}"
